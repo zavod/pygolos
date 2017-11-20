002861f5b35272a02df4c10d90e524e1503b1873
alp <aleksandrprotsyuk@Aleksandrs-MacBook-Pro.local>
2017-11-20 18:15:24 +0500

    fix static path

--- config/settings.py
+++ config/settings.py
@@ -139,5 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+UPLOAD_ROOT = os.path.join(BASE_DIR, 'media')
 
 from config.local_settings import *
