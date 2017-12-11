4fa7ede60ec978ef940841cf425a6293566834e6
Aleksandr Protsyuk <alp@zavode.ru>
2017-12-11 16:43:14 +0500

    baseProvider fix

--- apps/providers/provider.py
+++ apps/providers/provider.py
@@ -4,10 +4,9 @@ from apps.providers.steem.backend import SteemBackend
 
 class BaseProvider(object):
     def get_review_provider(self, review):
-        def get_blockchain_provider(self):
-            backend = None
-            if review.provider == 'golos':
-                backend = GolosBackend()
-            elif review.provider == 'steem':
-                backend = SteemBackend()
-            return backend
+        backend = None
+        if review.provider == 'golos':
+            backend = GolosBackend()
+        elif review.provider == 'steem':
+            backend = SteemBackend()
+        return backend
